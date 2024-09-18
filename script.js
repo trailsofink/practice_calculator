ebcc66d3e4bffaf9b7502529e68292d703eb63f8
trailsofink <trailsofink@gmail.com>
2024-09-18 15:20:01 -0500

    cleaning up the functions & adding more flexibility for more combos

--- script.js
+++ script.js
@@ -1,61 +1,70 @@
-// Figure out how to not let the equation buttons get pushed twice.
-// set each term to be an array starting with an operator
+// Add and operator onto the end of the answer.
 
 let calculator = document.querySelector('#calc');
-
 let calculation = [];
-
-let iscalculation = true;
-
+let equationHist = [];
 let isAnswer = false;
-
-let isEqual = false;
-
+let isDoubleOp = false;
+let isFirstNumber = true;
+let answer = null;
 const calcOutput = document.querySelector('#calcOutput');
-
 
 calculator.addEventListener('click', function(event) {
 
     let btnValue = event.target.ariaValueText;
     let btnValueShown = event.target.value;
     let btnValueStr = btnValueShown.toString();
-
-    function add() {
-        let term = {};
-        let i = calculation.length + 1;
-
-        term[`term${i}`] = [];
-        term[`term${i}`].push(btnValue);
-        calculation.push({[`term${i}`]: term[`term${i}`]});
-        calcOutput.lastElementChild.insertAdjacentHTML('beforeend', btnValueStr);
-    };
+    let lastTerm = calculation.length - 1;
 
     function clear() {
         calcOutput.innerHTML = '<span></span>';
-        calculation = []; 
-        calculation.push('Welcome');
+        calculation = [];
+        answer = null;
         isAnswer = false;
+        isDoubleOp = false;
+        isFirstNumber = true;
     }
 
-    if((!isAnswer) && (event.target.tagName === 'INPUT') && (event.target.ariaValueText !== "")) {
-        add();
-    } else if((isAnswer) && (event.target.tagName === 'INPUT') && (event.target.ariaValueText !== "")) {
+    function pushValue() {
+        calculation.push(btnValue);
+        calcOutput.lastElementChild.insertAdjacentHTML('beforeend', btnValueStr);
+    };
+
+
+    function addNum() {
+        if (!calculation.length) {
+            pushValue();
+            isFirstNumber = false;
+        } else {
+            let current = calculation[lastTerm];
+            calculation[lastTerm] = current + btnValue;
+            calcOutput.lastElementChild.insertAdjacentHTML('beforeend', btnValueStr);
+        }
+    };
+
+    if ((event.target.id === "number") && (!isAnswer)) {
+        addNum();
+        isDoubleOp = false;
+    } else if ((event.target.id === "number") && (isAnswer)) {
         clear();
-        add();
-    } else if ((!isAnswer) && ((!isEqual)) && (event.target.tagName === 'INPUT') && (event.target.value === "=")) {
+        addNum();
+    } else if ((event.target.id === "operator") && (!isFirstNumber) && (!isDoubleOp)) {
+        pushValue(); 
+        isDoubleOp = true;
+    } else if ((!isAnswer) && (event.target.id === 'equals')) {
         isAnswer = true; 
-        let makeAnser = calculation.join('');
-        let answer = eval(makeAnser);
+        let equation = calculation.join('');
+        answer = eval(equation);
         calcOutput.innerHTML = answer; 
-        isEqual = true;
-    } else if ((!isAnswer) && ((isEqual)) && (event.target.tagName === 'INPUT') && (event.target.value === "=")) {
-
-    } else if((isAnswer) && (event.target.tagName === 'INPUT') && (event.target.ariaValueText !== "")) {
-        clear();
-        add();
-    } else if ((isEqual)) {
-
-    } else if((event.target.tagName === 'INPUT') && (event.target.value === "AC")) {
+        equationHist.push(equation + '=' + answer);
+        document.getElementById('calcHist').insertAdjacentHTML('beforeend', "<span>" + equationHist + "</span>");
+        isFirstNumber = true;
+    } else if ((isAnswer) && (event.target.id === 'equals')) {
+        answerCalc = eval(String(answer + calculation[lastTerm]));
+        calcOutput.innerHTML = answerCalc; 
+        answer = answerCalc;
+        console.log(answer);
+    } else if (event.target.id === "clear") {
         clear(); 
-    } 
+    }
 });
