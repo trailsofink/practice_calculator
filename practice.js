40e35ed5ea14ab0d7f1d88bf3661b465f741cdd9
trailsofink <trailsofink@gmail.com>
2024-10-28 19:23:55 -0500

    progress

---
+++ practice.js
@@ -0,0 +1,30 @@
+for (let i = 1; i < 11; i++) {
+    if (i == 1) {
+        console.log("Gold medal");
+    } 
+    else if  (i == 2) {
+        console.log("Silver medal");
+    }
+    else if (i == 3) {
+        console.log("Bronze medal");
+    }
+    else {
+        console.log(i);
+    }
+}
+
+for (let i = 1; i < 11; i++) {
+    switch (i){
+        case 1:
+            console.log("Gold medal");
+            break;
+        case 2: 
+            console.log("Silver medal");
+            break;
+        case 3: 
+            console.log("Bronze medal");
+            break;
+        default:
+            console.log(i);
+    }
+}
--- script.js
+++ script.js
@@ -6,107 +6,124 @@ let lastValue = null;
 let lastOp = null;
 let isAnswer = false;
 
-const calculator = document.querySelector('#calc');
-const calcOutput = document.querySelector('#calcOutput');
-const calcHist = document.getElementById('calcHist');
-const inputs = calculator.querySelectorAll('input');
+const calculator = document.querySelector("#calc");
+const calcOutput = document.querySelector("#calcOutput");
+const calcHist = document.getElementById("calcHist");
+const inputs = calculator.querySelectorAll("input");
 
-inputs.forEach(input => {
-    input.addEventListener('click', () => {
-        let btnValue = input.ariaValueText;
-        let btnValueShown = input.value;
+inputs.forEach((input) => {
+  input.addEventListener("click", () => {
+    let btnValue = input.ariaValueText;
+    let btnValueShown = input.value;
 
-        function pushArr() {
-            calculation.push(btnValueShown);
-        }
+    function pushArr() {
+      calculation.push(btnValueShown);
+    }
 
-        function displayEquation() {
-            equation = calculation.join('');
-            calcOutput.innerText = equation;
-            lastValue = calculation[calculation.length - 1];
-            lastOp = calculation[calculation.length - 2];
-            isAnswer = false;
-        }
-        
-        function pushNum() {
-            if (isNaN(calculation[calculation.length - 1])) {
-                pushArr();
-            } else if (!isNaN(calculation[calculation.length - 1])){
-                calculation[calculation.length - 1] = calculation[calculation.length - 1] +  btnValueShown;
-            }
-            displayEquation(); 
-        }
+    function displayEquation() {
+      equation = calculation.join("");
+      calcOutput.innerText = equation;
+      lastValue = calculation[calculation.length - 1];
+      lastOp = calculation[calculation.length - 2];
+      isAnswer = false;
+    }
 
-        function pushOp() {
-           pushArr();
-           displayEquation();
-        }
+    function pushNum() {
+      if (isNaN(calculation[calculation.length - 1])) {
+        pushArr();
+      } else if (!isNaN(calculation[calculation.length - 1])) {
+        calculation[calculation.length - 1] =
+          calculation[calculation.length - 1] + btnValueShown;
+      }
+      displayEquation();
+    }
 
-        function plusOrMinus() {
-            if (!isNaN(calculation[calculation.length - 1])) {
-                calculation[calculation.length - 1] = "(-" + calculation[calculation.length - 1] + ')';
-                displayEquation();
-            } else if (isNaN(calculation[calculation.length - 1])) {
-                calculation[calculation.length - 1] = calculation[calculation.length - 1].replace('-', '').replace('(', '').replace(')', '');
-                displayEquation();
-            }
-        }
+    function pushOp() {
+      pushArr();
+      displayEquation();
+    }
 
-        function  percent() {
-            if (!isNaN(calculation[calculation.length - 1])) {
-                calculation[calculation.length - 1] = calculation[calculation.length - 1] + '%';
-                displayEquation();
-            } else if (!isNaN(calculation[calculation.length - 1])) {
-                calculation[calculation.length - 1] = calculation[calculation.length - 1].replace('%', '');
-                displayEquation();
-            }
-        }
+    function plusOrMinus() {
+      if (!isNaN(calculation[calculation.length - 1])) {
+        calculation[calculation.length - 1] =
+          "(-" + calculation[calculation.length - 1] + ")";
+        displayEquation();
+      } else if (isNaN(calculation[calculation.length - 1])) {
+        calculation[calculation.length - 1] = calculation[
+          calculation.length - 1
+        ]
+          .replace("-", "")
+          .replace("(", "")
+          .replace(")", "");
+        displayEquation();
+      }
+    }
 
-        function doCalculation() {
-            if (isAnswer) {
-               equation = answer + lastOp + lastValue;  
-            } 
-            equationHist.push(equation);
-            replaceSymbols = equation.replaceAll('x', '*').replaceAll('÷', '/').replaceAll('%', '/100'); 
-            answer = eval(replaceSymbols);
-            calcOutput.innerText = answer;
-            calcHist.insertAdjacentHTML('beforeend', "<p>" + equationHist + "</p>");
-            calculation = [];
-            calculation.push(answer);
-            equationHist = []; 
-            isAnswer = true;    
-        }
+    function percent() {
+      if (!isNaN(calculation[calculation.length - 1])) {
+        calculation[calculation.length - 1] =
+          calculation[calculation.length - 1] + "%";
+        displayEquation();
+      } else if (!isNaN(calculation[calculation.length - 1])) {
+        calculation[calculation.length - 1] = calculation[
+          calculation.length - 1
+        ].replace("%", "");
+        displayEquation();
+      }
+    }
 
-        function clear() {
-            calculation = [];
-            displayEquation();
-        }
+    function doCalculation() {
+      if (isAnswer) {
+        equation = answer + lastOp + lastValue;
+      }
+      equationHist.push(equation);
+      replaceSymbols = equation
+        .replaceAll("x", "*")
+        .replaceAll("÷", "/")
+        .replaceAll("%", "/100");
+      answer = eval(replaceSymbols);
+      calcOutput.innerText = answer;
+      calcHist.insertAdjacentHTML("beforeend", "<p>" + equationHist + "</p>");
+      calculation = [];
+      calculation.push(answer);
+      equationHist = [];
+      isAnswer = true;
+    }
 
-        function backspace() {
-            if (calculation[calculation.length -1]) {
-                calculation[calculation.length -1] = calculation[calculation.length - 1].slice(0, -1);
-                displayEquation();    
-            } else {
-                calculation.pop();
-                calculation[calculation.length -1] = calculation[calculation.length - 1].slice(0, -1);
-                displayEquation(); 
-            }
-        }
+    function clear() {
+      calculation = [];
+      displayEquation();
+    }
 
-        if (input.id === 'number') {
-            pushNum();
-        } else if (input.id === 'operator') {
-            pushOp();
-        } else if (input.id === 'equals') {
-            doCalculation();
-        } else if (input.id === 'plusOrMinus') {
-            plusOrMinus();
-        } else if (input.id === 'percent') {
-            percent();
-        } else if (input.id === 'clear') {
-            clear();
-        } else if (input.id === 'back') {
-            backspace();
-        }
-    })
+    function backspace() {
+      if (calculation[calculation.length - 1]) {
+        calculation[calculation.length - 1] = calculation[
+          calculation.length - 1
+        ].slice(0, -1);
+        displayEquation();
+      } else {
+        calculation.pop();
+        calculation[calculation.length - 1] = calculation[
+          calculation.length - 1
+        ].slice(0, -1);
+        displayEquation();
+      }
+    }
+
+    if (input.id === "number") {
+      pushNum();
+    } else if (input.id === "operator") {
+      pushOp();
+    } else if (input.id === "equals") {
+      doCalculation();
+    } else if (input.id === "plusOrMinus") {
+      plusOrMinus();
+    } else if (input.id === "percent") {
+      percent();
+    } else if (input.id === "clear") {
+      clear();
+    } else if (input.id === "back") {
+      backspace();
+    }
+  });
 });
